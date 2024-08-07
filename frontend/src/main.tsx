f688e75ba35d008738aac6801d57ae1bdaa21da7
Mattea <mattea.sax@gmail.com>
2024-08-07 20:17:45 +0200

    added queryclient provider to main.tsx file wrapping it around app, browserrouter and themecontext provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import ThemeContextProvider from "./contexts/ThemeContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeContextProvider>
-        <App />
-      </ThemeContextProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeContextProvider>
+          <App />
+        </ThemeContextProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
